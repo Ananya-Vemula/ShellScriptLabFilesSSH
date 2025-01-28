9d21c00579c9941aebd7290f7029c5e757e64277
Ananya-Vemula <145344093+Ananya-Vemula@users.noreply.github.com>
2025-01-28 14:07:09 +0530

    Update 02.sh

--- 02.sh
+++ 02.sh
@@ -1,5 +1,12 @@
-read -p "Type your first number" no1
-read -p "Type your second number" no2
-read -p "Type your thrid number"  no3
+#!/bin/bash
 
-echo "The sum of the three numbers is $(no1+no2+no3)"
+read -p "Type your first number: " no1
+read -p "Type your second number: " no2
+read -p "Type your third number: " no3
+
+# Calculate the sum
+sum=$((no1 + no2 + no3))
+
+# Print the result
+echo "The sum of the three numbers is $sum"
+
